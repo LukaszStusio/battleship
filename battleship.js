5d0bd06852f68080d40516a3d8b052bc02a378b6
LukaszStusio <lukasz.stusio@gmail.com>
2018-03-10 22:39:52 +0100

    debugging: it's easier to build a battleship than to create it

--- battleship.js
+++ battleship.js
@@ -3,12 +3,13 @@ var location2 = 4;
 var location3 = 5;
 
 var guess;
-var = hits = 0;
+var hits = 0;
 var guesses = 0;
 
 var isSunk = false;
 
 while (isSunk == false) {
+  //alert("test");
     guess = prompt("Ready, steady, fire! (choose the number from 0 to 6):");
     if (guess<0 || guess>6){
         alert("Please choose the correct number of cell!");
@@ -16,16 +17,18 @@ while (isSunk == false) {
         guesses = guesses + 1;
 
         if (guess == location1 || guess == location2 || guess == location3) {
+            alert("Hit!!!");
             hits = hits + 1;
-
             if (hits == 3) {
                 isSunk = true;
                 alert("You've just sunk my battleship! I hate you!");
             }
+        }else{
+            alert("Missed!");
         }
     }
 }
 
-var stats = "You needed " + guesses + " hits to put my battleship in the bottom of the ocean, " + "so your efficiency is: " + (3/guesses) ".";
+var stats = "You needed " + guesses + " hits to put my battleship in the bottom of the ocean, " + "so your efficiency is: " + (3/guesses) + ".";
 
 alert(stats);
