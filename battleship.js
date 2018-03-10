8ce6f57d936a8b2518e097f711a6d74ac141f0ee
LukaszStusio <lukasz.stusio@gmail.com>
2018-03-10 21:20:53 +0100

    hits check

--- battleship.js
+++ battleship.js
@@ -14,5 +14,9 @@ while (isSunk == false) {
         alert("Please choose the correct number of cell!");
     }else {
         guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+        }
     }
 }
