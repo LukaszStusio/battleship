8fc63c65923d622b41440f8d93a2b97a4a82445e
LukaszStusio <lukasz.stusio@gmail.com>
2018-03-10 20:30:16 +0100

    Step #1: loop preparation & data collection; new for me: prompt function

---
+++ battleship.js
@@ -0,0 +1,13 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+var guess;
+var = hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready, steady, fire! (choose the number from 0 to 6):")
+}
